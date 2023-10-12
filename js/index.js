2e78506ccd8f39aacc9a19cf2093b4a7a9412a86
José Alves <josealves3416@gmail.com>
2023-10-12 12:09:59 -0300

    Massa de contra peso fixo e distância variando

--- js/index.js
+++ js/index.js
@@ -1,42 +1,41 @@
 /*Constantes de distâncias*/
-const d1 = 7.5;
+const mCpeso = 4966.67;
 const d2 = 4;
 const d3 = 9.5;
 const d4 = 12.5;
 
 let cargaOut = document.getElementById("carga-massa");
 let cargaIn = document.getElementById("massa-carga");
-let contraPesoIn = document.getElementById("massa-cpeso");
-let contraPesoOut = document.getElementById("contra-peso");
+let d1Input = document.getElementById("distancia-cpeso");
+let d1Out = document.getElementById("distancia-cpeso-out");
 
-cargaOut.textContent = cargaIn.value;
-contraPesoOut.textContent = calculaContraPeso(cargaOut.value).toFixed(2);
-contraPesoIn.value = calculaContraPeso(cargaOut.value);
+cargaOut.textContent = cargaIn.value + " kg";
+d1Out.textContent = calculaDistanciaCpeso(cargaOut.value).toFixed(2) + " m";
+d1Input.value = calculaDistanciaCpeso(cargaOut.value);
 
-function calculaContraPeso(carga) {
-  return (1500*d3 + parseFloat(cargaIn.value)*d4 - 500*d2)/d1;
+function calculaDistanciaCpeso(carga) {
+  return (1500*d3 + parseFloat(cargaIn.value)*d4 - 500*d2)/mCpeso;
 }
 
-function calculaCarga(contraPeso) {
-  return (contraPeso*d1 - 1500*d3 + 500*d2)/d4;
+function calculaCarga(d1) {
+  return (mCpeso*d1 - 1500*d3 + 500*d2)/d4;
 }
 
 cargaIn.addEventListener("input", () => {
-  let cPeso = calculaContraPeso(cargaIn.value);
-  if(cPeso >= 0) {
-    cargaOut.textContent = cargaIn.value;
-    contraPesoOut.textContent = cPeso.toFixed(2);
-    contraPesoIn.value = cPeso;
+  let cPesoDistancia = calculaDistanciaCpeso(cargaIn.value);
+  cargaOut.textContent = cargaIn.value + " kg";    
+  if(cPesoDistancia >= 0) {
+    d1Out.textContent = cPesoDistancia.toFixed(2) + " m";
+    d1Input.value = cPesoDistancia;
   }
 })
 
-contraPesoIn.addEventListener("input", () => {
-  let cargaPeso = calculaCarga(contraPesoIn.value);
-  contraPesoOut.textContent = contraPesoIn.value;
+d1Input.addEventListener("input", () => {
+  let cargaPeso = calculaCarga(d1Input.value);
+  d1Out.textContent = d1Input.value + " m";
   if(cargaPeso < 0) {
     cargaPeso = 0;
   }
-  cargaOut.textContent = cargaPeso.toFixed(2);
+  cargaOut.textContent = cargaPeso.toFixed(2) + " kg";
   cargaIn.value = cargaPeso;
-
 })

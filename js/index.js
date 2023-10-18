9f08e7b74d8965db7a6bf42742c5ce019475ee83
José Alves <josealves3416@gmail.com>
2023-10-18 00:05:25 -0300

    Adição de sliders para centros de massa

--- js/index.js
+++ js/index.js
@@ -2,17 +2,28 @@
 const mCpeso = 4966.67;
 const d2 = 4;
 const d3 = 9.5;
-const d4 = 12.5;
+const d4 = 12.5; 
 
 let cargaOut = document.getElementById("carga-massa");
 let cargaIn = document.getElementById("massa-carga");
 let d1Input = document.getElementById("distancia-cpeso");
 let d1Out = document.getElementById("distancia-cpeso-out");
 let contraPesoBox = document.getElementById("contrapeso");
+let massaBCIn = document.getElementById("massa-bc-in");
+let massaBCOut = document.getElementById("massa-bc-out");
+let massaBDIn = document.getElementById("massa-bd-in");
+let massaBDOut = document.getElementById("massa-bd-out"); 
 
 cargaOut.textContent = cargaIn.value + " kg";
 d1Out.textContent = calculaDistanciaCpeso(cargaOut.value).toFixed(2) + " m";
 d1Input.value = calculaDistanciaCpeso(cargaOut.value);
+massaBCOut.textContent = massaBCIn.value + " kg";
+massaBDOut.textContent = massaBDIn.value + " kg";
+
+
+function calculaPosicao(distancia) {
+  return (40 + (Number(d1Input.value))/(10)*(-22.5));
+}
 
 function calculaDistanciaCpeso(carga) {
   return (1500*d3 + parseFloat(cargaIn.value)*d4 - 500*d2)/mCpeso;
@@ -28,6 +39,7 @@ cargaIn.addEventListener("input", () => {
   if(cPesoDistancia >= 0) {
     d1Out.textContent = cPesoDistancia.toFixed(2) + " m";
     d1Input.value = cPesoDistancia;
+    contraPesoBox.style.left = calculaPosicao(d1Input.value) + "%";
   }
 })
 
@@ -39,5 +51,15 @@ d1Input.addEventListener("input", () => {
   }
   cargaOut.textContent = cargaPeso.toFixed(2) + " kg"; 
   cargaIn.value = cargaPeso;
-  contraPesoBox.style.left = (40 + (Number(d1Input.value))/(10)*(-17)) + "%";
+  contraPesoBox.style.left = calculaPosicao(d1Input.value) + "%";
 })
+
+
+massaBCIn.addEventListener("input", () => {
+  massaBCOut.textContent = massaBCIn.value + " kg";
+})
+
+massaBDIn.addEventListener("input", () => {
+  massaBDOut.textContent = massaBDIn.value + " kg";
+})
+

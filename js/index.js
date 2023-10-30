c9d7317cdec003c66e0cf0c667dbfd5685b8c1e3
José Alves <josealves3416@gmail.com>
2023-10-30 18:06:15 -0300

    finalização

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+//Constate g
+g = 9.81;
+
 //Definição das variáveis presentes no arquivo .html
 let cargaOut = document.getElementById("carga-massa");
 let cargaIn = document.getElementById("massa-carga");
@@ -17,6 +20,9 @@ let dLanca = document.getElementById("distancia-lanca");
 let dLancaOut = document.getElementById("distancia-lanca-out");
 let dClanca = document.getElementById("distancia-clanca");
 let dClancaOut = document.getElementById("distancia-clanca-out");
+let buttonMode = document.getElementById("mode-btn");
+let sliders = document.getElementsByClassName("form-range");
+let torqueOut = document.getElementById("torque-output");
 
 //Atribuição de valores iniciais
 cargaOut.textContent = cargaIn.value + " kg";
@@ -29,6 +35,21 @@ dLancaOut.textContent = dLanca.value + "m";
 d1Out.textContent = calculaDistanciaCpeso(cargaOut.value).toFixed(2) + " m";
 d1Input.value = calculaDistanciaCpeso(cargaOut.value);
 
+//verifica qual modo está
+function freeMode() {
+  if(buttonMode.textContent == "Modo Livre")
+    return true;
+  else
+    return false;
+}
+
+//calcula momento resultante
+function momentoResultante() {
+  if(!freeMode())
+    return g*(-(massaBDIn.value)*(dLanca.value) - (cargaIn.value)*(calculaDistanciaCarga()) + (massaBCIn.value)*(dClanca.value) + (mCpeso.value)*(d1Input.value));
+  else
+    return g*(-(massaBDIn.value)*(dLanca.value) - (cargaIn.value)*(distanciaCarga.value) + (massaBCIn.value)*(dClanca.value) + (mCpeso.value)*(d1Input.value));
+}
 
 //Muda opacidade de texto
 function opacityOutput(value) {
@@ -43,76 +64,99 @@ function calculaPosicao(distancia, minPer, maxPer, minValue, maxValue, ) {
 
 //Calcula a distância do contra-peso
 function calculaDistanciaCpeso() {
-  return (parseFloat(massaBDIn.value)*parseFloat(dLanca.value) + parseFloat(cargaIn.value)*parseFloat(distanciaCarga.value) - parseFloat(massaBCIn.value)*parseFloat(dClanca.value))/mCpeso.value;
+  return ((massaBDIn.value)*(dLanca.value) + (cargaIn.value)*(distanciaCarga.value) - (massaBCIn.value)*(dClanca.value))/mCpeso.value;
 }
 
 //Calcula a distância da carga
 function calculaDistanciaCarga() {
-  return (parseFloat(mCpeso.value)*parseFloat(d1Input.value) - parseFloat(massaBDIn.value)*parseFloat(dLanca.value) + parseFloat(massaBCIn.value)*parseFloat(dClanca.value))/parseFloat(cargaIn.value);
+  return ((mCpeso.value)*(d1Input.value) - (massaBDIn.value)*(dLanca.value) + parseFloat(massaBCIn.value)*(dClanca.value))/(cargaIn.value);
 }
 
 /*Varia distância da carga e calcula distância do contra-peso*/
 distanciaCarga.addEventListener("input", () => {  
   distanciaCargaOut.textContent = distanciaCarga.value + "m"
   distanciaCargaOut.style.color = "black";
-  cargaBox.style.left = calculaPosicao(distanciaCarga.value,40,70,1,25) + "%";
-  opacityOutput(1);
-  d1Input.value = calculaDistanciaCpeso();
-  d1Out.textContent = calculaDistanciaCpeso().toFixed(2) + "m";
   contraPesoBox.style.left = calculaPosicao(d1Input.value, 52, 29, 1, 25) + "%";
-  if(calculaDistanciaCpeso() < 0) 
-    d1Out.style.color = "red";
-  else 
-    d1Out.style.color = "black";
+  if(!freeMode()) {
+    cargaBox.style.left = calculaPosicao(distanciaCarga.value,40,70,1,25) + "%";
+    opacityOutput(1);
+    d1Input.value = calculaDistanciaCpeso();
+    d1Out.textContent = calculaDistanciaCpeso() + "m";
+    
+    if(calculaDistanciaCpeso() < 0 || calculaDistanciaCpeso() > 25) 
+      d1Out.style.color = "red";
+    else 
+      d1Out.style.color = "black";}
 })
 
 /* Varia distância do contra-peso e calcula distância da carga */
 d1Input.addEventListener("input", () => {
   d1Out.style.color = "black";
   d1Out.textContent = d1Input.value + " m";
-  opacityOutput(1);
-  contraPesoBox.style.left = calculaPosicao(d1Input.value, 52, 29, 1, 25) + "%";
-  distanciaCarga.value = calculaDistanciaCarga();
-  distanciaCargaOut.textContent = calculaDistanciaCarga().toFixed(2) + "m";
   cargaBox.style.left = calculaPosicao(distanciaCarga.value,40,70,1,25) + "%";
-  if(calculaDistanciaCarga() < 0) 
-    distanciaCargaOut.style.color = "red";
-  else 
-  distanciaCargaOut.style.color = "black";
+  if(!freeMode()) {
+    opacityOutput(1);
+    contraPesoBox.style.left = calculaPosicao(d1Input.value, 52, 29, 1, 25) + "%";
+    distanciaCarga.value = calculaDistanciaCarga();
+    distanciaCargaOut.textContent = calculaDistanciaCarga() + "m";
+    if(calculaDistanciaCarga() < 0) 
+      distanciaCargaOut.style.color = "red";
+    else 
+    distanciaCargaOut.style.color = "black";}
 })
 
 //Carga input
 cargaIn.addEventListener("input", () => {
   cargaOut.textContent = cargaIn.value + "kg";
-  opacityOutput(0.2);
+  if(!freeMode())
+    opacityOutput(0.2);
 })
 
 //Massa contra-lança input
 massaBCIn.addEventListener("input", () => {
   massaBCOut.textContent = massaBCIn.value + " kg";
-  opacityOutput(0.2)
+  if(!freeMode())  
+    opacityOutput(0.2)
 })
 
 //Massa lança input
 massaBDIn.addEventListener("input", () => {
   massaBDOut.textContent = massaBDIn.value + " kg";
-  opacityOutput(0.2);
+  if(!freeMode())
+    opacityOutput(0.2);
 })
 
 //Massa contra peso input
 mCpeso.addEventListener("input", () => {
   mCpesoOut.textContent = mCpeso.value + " kg";
-  opacityOutput(0.2);
+  if(!freeMode())
+    opacityOutput(0.2);
 })
 
 //Massa contra-lança input
 dClanca.addEventListener("input", () => {
   dClancaOut.textContent = dClanca.value + "m";
-  opacityOutput(0.2)
+  if(!freeMode())
+    opacityOutput(0.2)
 })
 
 //Massa lança input
 dLanca.addEventListener("input", () => {
   dLancaOut.textContent = dLanca.value + "m";
-  opacityOutput(0.2);
+  if(!freeMode())
+    opacityOutput(0.2);
 })
+
+//Muda de modo
+buttonMode.addEventListener("click", () => {
+  if(!freeMode()) {
+    buttonMode.textContent = "Modo Livre";
+    opacityOutput(1)
+  } 
+  else
+    buttonMode.textContent = "Equilíbrio";
+})
+
+for(i = 0; i < sliders.length; i++) {
+    sliders[i].addEventListener("input",() =>{torqueOut.textContent = "Momento Resultante: " + Math.round(momentoResultante()) + "N.m"});
+  }
